71ff3e4dd4609fc5e4196d29eb0818649cd49b4e
Dilara Ahretlikoğlu <97093117+DilaraAhretlikoglu@users.noreply.github.com>
2023-07-10 15:40:43 +0300

    for loop
    for loop

---
+++ For_Döngüsü/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace For_Döngüsü
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Bir sayi girin:");
+            // int sayi=int.Parse(Console.ReadLine());
+            // for (int i = 1; i <= sayi; i++)
+            // {
+            //     if(i%2==1)
+            //         Console.WriteLine(i);
+            // }
+            int ciftToplam=0,tekToplam=0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if(i%2==0)
+                {
+                    ciftToplam+=i;
+                }
+                else{
+                    tekToplam+=i;
+                }
+            }
+            Console.WriteLine("Tek sayilarin toplami:"+tekToplam);
+            Console.WriteLine("Cift sayilarin toplami:"+ciftToplam);
+
+        }
+    }
+}
