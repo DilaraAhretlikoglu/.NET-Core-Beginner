d86b92625efde13fa4bf8418a2204955f36e6ddc
Dilara Ahretlikoğlu <97093117+DilaraAhretlikoglu@users.noreply.github.com>
2023-07-12 19:12:35 +0300

    array
    array

--- While_Foreach/Program.cs
+++ While_Foreach/Program.cs
@@ -1,34 +1,35 @@
 ﻿using System;
 namespace While_Foreach
 {
-    class Program
-    {
+     class Program
+     {
         static void Main(string[] args)
         {
-            //while
-            Console.WriteLine("Bir sayi giriniz:");
-            int sayi=int.Parse(Console.ReadLine());
-            int sayac=1;
-            int toplam=0;
-            while (sayac<=sayi)
-            {
-               toplam+=sayac;
-               sayac ++;
-            }
-            Console.WriteLine(toplam/sayi);
+             //while
+             Console.WriteLine("Bir sayi giriniz:");
+             int sayi=int.Parse(Console.ReadLine());
+             int sayac=1;
+             int toplam=0;
+             while (sayac<=sayi)
+             {
+                toplam+=sayac;
+                sayac ++;
+             }
+             Console.WriteLine(toplam/sayi);
 
-            char karakter='a';
-            while(karakter<'z')
-            {
-                Console.WriteLine(karakter);
-                karakter++;
-            }
-            //Foreach
-            string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
-            foreach (var i in arabalar)
-            {
-                Console.WriteLine(i);
-            }
+             char karakter='a';
+             while(karakter<'z')
+             {
+                 Console.WriteLine(karakter);
+                 karakter++;
+             }
+             //Foreach
+             string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+             foreach (var i in arabalar)
+             {
+                 Console.WriteLine(i);
+             }
+            
         }
     }
 }
